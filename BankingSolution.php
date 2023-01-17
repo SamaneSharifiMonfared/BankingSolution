7e660de4a99f6b28e9610bc024f4aabf86309bfb
SamaneSharifiMonfared <samane.sharify@gmail.com>
2023-01-17 13:40:21 +0000

    class decleration and valiation of commit

--- BankingSolution.php
+++ BankingSolution.php
@@ -1,4 +1,28 @@
 <?php
 
+include_once "Classes/Classes.php";
+
+
+$bankingSolution = new bankingSolution();
+
+
+while(1){
+
+    print_r("Welcome to my Banking Solution, Programmed by Samane Monfared: \n");
+    $Command = readline("Please Enter Your Command Below Or For Exit Enter x: \n ");
+    if($Command=="x"){
+        break;
+    }
+
+    $validation=$bankingSolution->commandValidationAndRun($Command);
+
+    if(!$validation){
+        print_r("This Command is not Valid! Try again! \n");
+        continue;
+    }
+}
+
+
+
 
 ?>
--- Classes/Classes.php
+++ Classes/Classes.php
@@ -1,1 +1,37 @@
 <?php
+
+
+class bankingSolution{
+
+    private $command;
+    private $validationResult=False;
+
+//    private function __construct() {}
+
+    public function commandValidationAndRun($Command){
+
+        $Commands=explode(" ",$Command);
+
+        if($Commands[0]=="Create"){
+            print_r($Commands);
+
+        }
+
+//        $this->validationResult=True;
+
+        if($this->validationResult){
+            $this->command=$Command;
+        }
+        return $this->validationResult;
+
+    }
+
+    private function createAccount(){
+
+    }
+
+
+
+
+
+}
