ee6fc3f3bb01b8cd6ec35fd13f6eff9ee840f163
SamaneSharifiMonfared <samane.sharify@gmail.com>
2023-01-17 18:21:15 +0000

    database

--- app/Database/queries.php
+++ app/Database/queries.php
@@ -4,8 +4,8 @@ $sqlConn=new sqlConnection();
 
 $conn = $sqlConn->OpenCon();
 
-$query_users="SELECT * FROM .users WHERE deleted = 'N' ;";
+$query_accounts="SELECT * FROM bankdataset.accounts WHERE deleted = 'N' ;";
 
-$users=$sqlConn->mysql_fetch($conn,$query_users);
+$accounts=$sqlConn->mysql_fetch($conn,$query_accounts);
 
 
